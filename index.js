8af388349836c12d3058d14f7c7dfdc8103eb24a
labgambino <113803192+labgambino@users.noreply.github.com>
2023-02-26 23:22:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function ir (){
+    
+var c=2244; 
+var u="attec"; 
+    if(document.form.password.value==c && document.form.login.value==u){
+    alert ("ATTEC: Acceso permitido al EXPEDIENTE CONTR 2022 922907-Osuna-La Puebla de Cazalla. \nTodos los Derechos reservados."); 
+window.location="hiperenlopag.html";} 
+    
+    else { 
+        alert("Por favor, ingresa tu usuario y contraseña correctos");
+    }
+     }
